7da82a41e9f425cfc0ef62918221fce58cbf86a2
Jason Jackson <jake1164@hotmail.com>
2025-10-09 06:58:04 -0400

    Added starting point and .gitignore.

---
+++ cli/iptv/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace iptv;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var cmdline = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        Console.WriteLine("Hello World!");
+    }
+}
